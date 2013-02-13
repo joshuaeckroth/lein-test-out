d2b3a50aa78e5ede3c7f5969bb956bfba1341fa3
Michael Rowe <mrowe@mojain.com>
2013-02-13 17:30:02 +1100

    Upgrade clojure/tools.namespace

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject lein2-test-out "0.2.1"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.namespace "0.1.3"]])
+                 [org.clojure/tools.namespace "0.2.2"]])
