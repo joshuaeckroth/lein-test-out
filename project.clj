8db6a38b3ecf72a6065981099be2ef0606d72807
Michael Rowe <mrowe@mojain.com>
2013-02-13 17:29:01 +1100

    These are runtime deps, not just build time

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject lein2-test-out "0.2.1"
-  :dev-dependencies [[org.clojure/clojure "1.4.0"]
-                     [org.clojure/tools.namespace "0.1.3"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.namespace "0.1.3"]])
